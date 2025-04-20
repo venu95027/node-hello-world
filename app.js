0fd16714d99ded51507a1dfb0ec751c9744827eb
venugithub <32477642+venu95027@users.noreply.github.com>
2025-04-20 08:18:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = 3000; // Changed from 8080 to 3000
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('Hello World from IBM Cloud Essentials!');
-})
+});
 
 app.listen(port, host);
 console.log(`Running on http://${host}:${port}`);
